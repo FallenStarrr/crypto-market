c00cab153cc3b5d14d410cbb3d3a349d70db1d6c
vladof <v.fedorchenko@shinhanfinance.kz>
2024-03-07 11:27:54 +0600

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+
+const headerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 60,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+};
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 'calc(100vh - 60px)',
+  color: '#fff',
+  backgroundColor: '#001529',
+};
+const siderStyle = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#1677ff',
+};
+
+
+
+
 export default function App() {
-  return <h1>React Crypto App</h1>
+  return <Layout>
+  <Layout.Header style={headerStyle}>Header</Layout.Header>
+  <Layout>
+    <Layout.Sider width="25%" style={siderStyle}>
+      Sider
+    </Layout.Sider>
+    <Layout.Content style={contentStyle}>Content</Layout.Content>
+  </Layout>
+</Layout>
 }
