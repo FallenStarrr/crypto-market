2237130cada147c54c6b24e1f49651a8bd7c11a8
vladof <v.fedorchenko@shinhanfinance.kz>
2024-03-07 11:37:25 +0600

    refactor components for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,23 @@
 import React from 'react';
 import { Layout } from 'antd';
+import AppHeader from './components/layout/AppHeader';
+import AppSider from './components/layout/AppSider';
+import AppContent from './components/layout/AppContent';
 
 
-const headerStyle = {
-  textAlign: 'center',
-  color: '#fff',
-  height: 60,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
-};
-const contentStyle = {
-  textAlign: 'center',
-  minHeight: 'calc(100vh - 60px)',
-  color: '#fff',
-  backgroundColor: '#001529',
-};
-const siderStyle = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#1677ff',
-};
+
 
 
 
 
 export default function App() {
-  return <Layout>
-  <Layout.Header style={headerStyle}>Header</Layout.Header>
+  return (
   <Layout>
-    <Layout.Sider width="25%" style={siderStyle}>
-      Sider
-    </Layout.Sider>
-    <Layout.Content style={contentStyle}>Content</Layout.Content>
+          <AppHeader/>
+          <Layout>
+            <AppSider/>
+            <AppContent/>
+          </Layout>
   </Layout>
-</Layout>
+  )
 }
