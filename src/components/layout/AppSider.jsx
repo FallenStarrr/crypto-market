c380d6f5ddd3eab0dbbeb0861a089913633db8f7
vladof <v.fedorchenko@shinhanfinance.kz>
2024-03-07 11:57:05 +0600

    new icons for sider

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,18 +1,34 @@
-import { Layout } from 'antd';
-
+import { Layout, Card, Statistic } from 'antd';
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
 
 const siderStyle = {
-    textAlign: 'center',
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#1677ff',
+    padding: '1rem'
   };
 
 
 export default function AppSider() {
     return (
     <Layout.Sider width="25%" style={siderStyle}>
-      Sider
+          <Card>
+                <Statistic
+                    title="Active"
+                    value={11.28}
+                    precision={2}
+                    valueStyle={{ color: '#3f8600' }}
+                    prefix={<ArrowUpOutlined />}
+                    suffix="%"
+                />
+          </Card>
+          <Card>
+                <Statistic
+                title="Idle"
+                value={9.3}
+                precision={2}
+                valueStyle={{ color: '#cf1322' }}
+                prefix={<ArrowDownOutlined />}
+                suffix="%"
+                />
+          </Card>
     </Layout.Sider>
     )
 }
