d9e4e2c2d3edba177b7ed32dd81ba34784255cea
FallenStarrr <juzopigeon@gmail.com>
2024-03-08 11:40:53 +0600

    styles header

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -2,11 +2,13 @@ import { Layout } from 'antd';
 
 
 const headerStyle = {
+    width: '100%',
     textAlign: 'center',
-    color: '#fff',
     height: 60,
-    paddingInline: 48,
-    lineHeight: '64px',
+    padding: '1rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     backgroundColor: '#4096ff',
   };
 
