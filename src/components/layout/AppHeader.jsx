27107243e81fd0897f7f762f5d8c96732038e448
FallenStarrr <juzopigeon@gmail.com>
2024-03-08 12:02:57 +0600

    select for header

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,5 +1,5 @@
-import { Layout, Select, Button } from 'antd';
-
+import { Layout, Select, Button, Space } from 'antd';
+import { useCrypto } from '../../context/crypto-context';
 
 const headerStyle = {
     width: '100%',
@@ -11,38 +11,9 @@ const headerStyle = {
     alignItems: 'center',
   };
 
-  const handleChange = (value) => {
-    console.log(`selected ${value}`);
-  };
-  
-  const options = [
-    {
-      label: 'China',
-      value: 'china',
-      emoji: '🇨🇳',
-      desc: 'China (中国)',
-    },
-    {
-      label: 'USA',
-      value: 'usa',
-      emoji: '🇺🇸',
-      desc: 'USA (美国)',
-    },
-    {
-      label: 'Japan',
-      value: 'japan',
-      emoji: '🇯🇵',
-      desc: 'Japan (日本)',
-    },
-    {
-      label: 'Korea',
-      value: 'korea',
-      emoji: '🇰🇷',
-      desc: 'Korea (韩国)',
-    },
-  ];
-
+ 
 export default function AppHeader() {
+    const { crypto } = useCrypto()
     return ( <Layout.Header style={headerStyle}>
       <Select
     style={{
@@ -50,13 +21,21 @@ export default function AppHeader() {
     }}
     
     
-    onChange={handleChange}
+
     value="press / to open"
     optionLabelProp="label"
-    options={options}
+    options={crypto.map((coin) => ({
+      label: coin.name,
+      value: coin.id,
+      icon: coin.icon
+    }))}
     optionRender={(option) => (
       <Space>
-          <img/> bitcoin
+          <img 
+          style={{width: 20}}
+          src={option.data.icon} 
+          alt={option.data.label}/> 
+          {option.data.label}
       </Space>
     )}
   />
