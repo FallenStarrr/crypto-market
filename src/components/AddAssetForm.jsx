2371dd2f3e0f62f299408ea28b7580967f073523
FallenStarrr <juzopigeon@gmail.com>
2024-03-08 17:20:57 +0600

    from components

--- src/components/AddAssetForm.jsx
+++ src/components/AddAssetForm.jsx
@@ -1,3 +1,111 @@
+import { Select, Space, Typography, Flex, Divider,  Form, Input, InputNumber, Button, DatePicker } from "antd"
+import { useState } from "react"
+import { useCrypto } from "../context/crypto-context"
+
 export default function AddAssetForm() {
-  return <form action="">FORM ASSET</form>
+  const [coin , setCoin ] = useState(null)
+  const {crypto}= useCrypto()
+  if (!coin) {
+    return (
+      <Select
+      style={{width: '100%'}}
+      onSelect={(v) => setCoin(crypto.find((c) => c.id == v))}
+      placeholder="Select coin"
+      options={crypto.map((coin) => ({
+        label: coin.name,
+        value: coin.id,
+        icon: coin.icon
+      }))}
+      optionRender={(option) => (
+        <Space>
+            <img 
+            style={{width: 20}}
+            src={option.data.icon} 
+            alt={option.data.label}/> 
+            {option.data.label}
+        </Space>
+      )}
+    />
+    )
+  }
+  
+ function onFinish(values) {
+    console.log(values)
+ }
+
+  return (
+  <Form
+    name="basic"
+    labelCol={{
+      span: 4,
+    }}
+    wrapperCol={{
+      span: 10,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+    initialValues={{
+    }}
+    onFinish={onFinish}
+  >
+       <Flex align='center'>
+        <img
+         src={coin.icon} 
+         alt={coin.name}
+          style={{ width: 40, marginRight: 10}}
+         />
+        <Typography.Title level={2} style={{margin: 0}}>
+           {coin.name}
+        </Typography.Title>
+  </Flex>
+  <Divider/>
+
+  
+    <Form.Item
+      label="Amount"
+      name="amount"
+      rules={[
+        {
+          required: true,
+          type: 'number',
+          min: 0,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <InputNumber style={{width: '100%'}}/>
+    </Form.Item>
+
+    <Form.Item
+      label="Price"
+      name="price"   
+    >
+     <InputNumber disabled style={{width: '100%'}}/>
+    </Form.Item>
+
+    <Form.Item
+      label="Date & Time"
+      name="date"   
+    >
+     <DatePicker showTime/>
+    </Form.Item>
+
+    <Form.Item
+      label="Total"
+      name="total"   
+    >
+     <InputNumber disabled style={{width: '100%'}}/>
+    </Form.Item>
+
+    
+
+    <Form.Item>
+      <Button type="primary" htmlType="submit">
+             Add Asset
+      </Button>
+    </Form.Item>
+  </Form>
+  
+  )
 }
--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -21,7 +21,7 @@ export default function AppHeader() {
     const [select, setSelect] = useState(false)
     const [coin, setCoin] = useState(null)
     const [modal, setModal] = useState(false)
-    const [drawer, setDrawer] = useState(false)
+    const [drawer, setDrawer] = useState(true)
 
   
     useEffect(() => {
@@ -75,6 +75,7 @@ export default function AppHeader() {
       </Modal>
 
       <Drawer
+      destroyOnClose
       width={600}
       title="Add Asset" 
       onClose={() => setDrawer(false)} 
