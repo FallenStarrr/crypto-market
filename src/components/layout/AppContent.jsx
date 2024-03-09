c291091aacef1101be5c7a67e4f2f7f81f4d98fc
FallenStarrr <juzopigeon@gmail.com>
2024-03-09 13:45:10 +0600

    crypto map and app content

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,4 +1,6 @@
 import { Layout } from 'antd';
+import Typography from 'antd/es/typography/Typography';
+import { useCrypto } from '../../context/crypto-context';
 
 
 
@@ -13,7 +15,24 @@ const contentStyle = {
 
 
 export default function AppContent() {
+    const { assets, crypto } =  useCrypto()
+
+    const cryptoPriceMap = crypto.reduce((acc, c) => {
+        acc[c.id] = c.price
+        return acc
+    }, {})
+
+
     return (
-        <Layout.Content style={contentStyle}>Content</Layout.Content>
+        <Layout.Content style={contentStyle}>
+            <Typography.Title level={3}
+                style={{textAlign: 'left', color: 'white'}}
+            >
+                        Portfolio: {assets.map(asset => asset.amount * cryptoPriceMap[asset.id]
+                        )
+                        .reduce((acc, v) => acc += v ,0)
+                        .toFixed(2)}$
+            </Typography.Title>
+        </Layout.Content>
     )
 }
--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -21,7 +21,7 @@ export default function AppHeader() {
     const [select, setSelect] = useState(false)
     const [coin, setCoin] = useState(null)
     const [modal, setModal] = useState(false)
-    const [drawer, setDrawer] = useState(false)
+    const [drawer, setDrawer] = useState(false) 
 
   
     useEffect(() => {
